75731b959436032da55a3a0a89de1910b07cdae9
Shrey Kejriwal <shreykejriwal@gmail.com>
2020-11-16 19:11:05 +0530

    add dashboard subapp

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
-import App from './App';
+import App from '../../auth/src/App';
 
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
